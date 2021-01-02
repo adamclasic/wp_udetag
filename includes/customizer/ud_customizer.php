5f0227545996069a411f22450b6fc7344f2d2267
adam allalou <aabou9@gmail.com>
2021-01-02 09:06:25 +0100

    update site identity section

--- includes/customizer/ud_customizer.php
+++ includes/customizer/ud_customizer.php
@@ -1,6 +1,10 @@
 <?php
 function ud_customizer($wp_customize)
 {
+  // echo '<pre>';
+  // var_dump($wp_customize);
+  // echo '<pre/>';
+  $wp_customize->get_section('title_tagline')->title = 'General';
   $wp_customize->add_panel('my_panel', [
     'title' => 'Our Custom Panel',
     'description' => '<p>This is a man made panel</p>'
