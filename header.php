e07624445061c4b02d58c16336fe8d98ba727026
adam allalou <aabou9@gmail.com>
2021-02-02 17:02:47 +0100

    update shopping cart with php

--- header.php
+++ header.php
@@ -245,32 +245,87 @@
                   <h4>Shopping Cart</h4>
                 </div>
                 <div class="top-cart-items">
+
+
+                <?php foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+                    $_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+                    $product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+
+                    if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+                      $product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
+                      ?>
+
+
                   <div class="top-cart-item clearfix">
                     <div class="top-cart-item-image">
-                      <a href="#"><img src="images/shop/small/1.jpg" /></a>
+                      <?php
+                        $thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+
+                        if ( ! $product_permalink ) {
+                          echo $thumbnail; // PHPCS: XSS ok.
+                        } else {
+                          printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
+                        }
+                      ?>
                     </div>
                     <div class="top-cart-item-desc">
-                      <a href="#">Blue Round-Neck Tshirt</a>
-                      <span class="top-cart-item-price">$19.99</span>
-                      <span class="top-cart-item-quantity">x 2</span>
+                      <?php
+                        if ( ! $product_permalink ) {
+                          echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
+                        } else {
+                          echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
+                        }
+
+                        do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
+
+                        // Meta data.
+                        echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
+
+                        // Backorder notification.
+                        if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
+                          echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
+                        }
+                      ?>
+                      <span class="top-cart-item-price">
+                        <?php
+                          echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+                        ?>
+                      </span>
+                      <span class="top-cart-item-quantity">
+                        <?php
+                          if ( $_product->is_sold_individually() ) {
+                            $product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
+                          } else {
+                            $product_quantity = woocommerce_quantity_input(
+                              array(
+                                'input_name'   => "cart[{$cart_item_key}][qty]",
+                                'input_value'  => $cart_item['quantity'],
+                                'max_value'    => $_product->get_max_purchase_quantity(),
+                                'min_value'    => '0',
+                                'product_name' => $_product->get_name(),
+                              ),
+                              $_product,
+                              false
+                            );
+                          }
+
+                          echo $cart_item['quantity'];
+                          // echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
+                        ?>
+                      </span>
                     </div>
                   </div>
-                  <div class="top-cart-item clearfix">
-                    <div class="top-cart-item-image">
-                      <a href="#"><img src="images/shop/small/6.jpg" /></a>
-                    </div>
-                    <div class="top-cart-item-desc">
-                      <a href="#">Light Blue Denim Dress</a>
-                      <span class="top-cart-item-price">$24.99</span>
-                      <span class="top-cart-item-quantity">x 3</span>
-                    </div>
-                  </div>
-                </div>
-                <div class="top-cart-action clearfix">
-                  <span class="fleft top-checkout-price">$114.95</span>
-                  <button class="button button-3d button-small nomargin fright">
-                    View Cart
-                  </button>
+
+
+                      <?php
+                    }
+                  }
+                  ?>
+                  <a href="<?php site_url('cart') ;?>">
+                    <button  class="button button-3d button-small nomargin fright">
+                      View Cart
+                    </button>
+                  </a>
                 </div>
               </div>
             </div><!-- #top-cart end -->
