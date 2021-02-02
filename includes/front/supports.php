4c6b104d5c00c807c0ee2ee7d114d014d729dafe
adam allalou <aabou9@gmail.com>
2021-02-02 08:21:07 +0100

    add woocomerce support

--- includes/front/supports.php
+++ includes/front/supports.php
@@ -4,6 +4,7 @@ function supports()
   add_theme_support( 'post-thumbnails' );
   add_theme_support( 'title-tag' );
   add_theme_support( 'custom-logo' );
+  add_theme_support( 'woocomerce' );
 
   register_nav_menu('primary', 'Primary Menu');
   register_nav_menu('secondary', 'Secondary Menu');
